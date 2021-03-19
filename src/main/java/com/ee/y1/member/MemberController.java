05c1d742157fb96c6b71641681aa473f6014d828
yeon-a-choi <banmu_@naver.com>
2021-03-19 09:39:21 +0900

    memberPage
    test

--- src/main/java/com/ee/y1/member/MemberController.java
+++ src/main/java/com/ee/y1/member/MemberController.java
@@ -2,8 +2,10 @@ package com.ee.y1.member;
 
 import javax.servlet.http.HttpServletRequest;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -75,7 +77,7 @@ public class MemberController {
 	
 	
 	@RequestMapping(value="/member/memberLogin", method=RequestMethod.POST)
-	public void memberLogin2(HttpServletRequest request) throws Exception {
+	public String memberLogin2(HttpServletRequest request) throws Exception {
 		
 		String id = request.getParameter("id");
 		String pw = request.getParameter("pw");
@@ -87,6 +89,10 @@ public class MemberController {
 		memberDTO = memberService.memberLogin(memberDTO);
 		
 		System.out.println(memberDTO);
+		
+		request.setAttribute("dto", memberDTO);
+		
+		return "member/memberPage";
 		
 	}
 
---
+++ src/main/webapp/WEB-INF/views/member/memberPage.jsp
@@ -0,0 +1,20 @@
+<%@page import="com.ee.y1.member.MemberDTO"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%
+	MemberDTO memberDTO = (MemberDTO)request.getAttribute("dto");
+%>    
+    
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+
+	<h1>Member Page</h1>
+	<h1><%= memberDTO.getEmail() %></h1>
+
+</body>
+</html>
